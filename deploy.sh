2b61d6c151f254f48f90439f3f69c2eafbab0c18
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-12-12 16:03:06 +0200

    travis config bump

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "***********[ Pulling latest images ]***********"
-ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p $FIKSU_SSH_PORT "docker pull codecontrol/fiksu-frontend:dev-$TRAVIS_BUILD_NUMBER"
-ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p $FIKSU_SSH_PORT "docker pull codecontrol/fiksu-backend:dev-$TRAVIS_BUILD_NUMBER"
+ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p 20150 "docker pull codecontrol/fiksu-frontend:dev-$TRAVIS_BUILD_NUMBER"
+ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p 20150 "docker pull codecontrol/fiksu-backend:dev-$TRAVIS_BUILD_NUMBER"
 echo "***********[ Updating images]***********"
 BACKEND="docker service update --env-add VERSION='$TRAVIS_BUILD_NUMBER' --image codecontrol/fiksu-backend:dev-'${TRAVIS_BUILD_NUMBER}' fiksu_backend"
 FRONTEND="docker service update --env-add VERSION='$TRAVIS_BUILD_NUMBER' --image codecontrol/fiksu-frontend:dev-'${TRAVIS_BUILD_NUMBER}' fiksu_frontend"
-ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p $FIKSU_SSH_PORT $FRONTEND
-ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p $FIKSU_SSH_PORT $BACKEND
+ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p 20150 $FRONTEND
+ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p 20150 $BACKEND
 echo "***********[ Cleanup ]***********"
 CLEANUP="docker system prune -f"
-ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p $FIKSU_SSH_PORT $CLEANUP
+ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p 20150 $CLEANUP
