29b26f262089ad95dbafbaa1919470449ec9a837
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-12-12 13:25:05 +0200

    travis and deploy script changes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "***********[ Pulling latest images ]***********"
 ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p 20150 "docker pull codecontrol/fiksu-frontend:dev-$TRAVIS_BUILD_NUMBER'"
-ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p 20150 "docker pull fixu/backend:dev-$TRAVIS_BUILD_NUMBER'"
+ssh $SERVER_USERNAME@fiksu.codecontrol.fi -p 20150 "docker pull codecontrol/fiksu-backend:dev-$TRAVIS_BUILD_NUMBER'"
 echo "***********[ Updating images]***********"
 BACKEND="sudo docker service update --env-add VERSION='$TRAVIS_BUILD_NUMBER' --image codecontrol/fiksu-backend:dev-'${BUILD_NUMBER}' fiksu_backend"
 FRONTEND="sudo docker service update --env-add VERSION='$TRAVIS_BUILD_NUMBER' --image codecontrol/fiksu-frontend:dev-'${BUILD_NUMBER}' fiksu_frontend"
