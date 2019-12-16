09e023f672a5b8ec594144e9ead33bb0b8be37d0
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-12-16 14:44:33 +0200

    fix navbar badges

--- frontend/src/components/molecules/accountNavbar.js
+++ frontend/src/components/molecules/accountNavbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import AccountHeader from '../atoms/headerAccount'
 import MenuList from '../atoms/menuList'
 import MenuItem from '../atoms/menuItem'
-import { GET_USER_FAMILIES, GET_USER_GROUPS } from '../../graphqlQueries'
+import { GET_USER_FAMILIES, GET_USER_INVITED_GROUPS, GET_USER_PENDING_FAMILIES } from '../../graphqlQueries'
 import { useQuery } from "@apollo/react-hooks"
 import {
   withRouter
@@ -17,19 +17,18 @@ const AccountNavbar = props => {
       id: localStorage.getItem('userId')
     }
   })
-  const { loading: groupLoading, error: groupError, data: groupData } = useQuery(GET_USER_GROUPS, {
-    variables: {
-      id: localStorage.getItem('userId')
-    }
-  })
+  const { loading: invitationsLoading, error: invitationsError, data: invitationsData } = useQuery(GET_USER_PENDING_FAMILIES)
+
+  const { loading: groupInvitationsLoading, error: groupInvitationsError, data: groupInvitationsData } = useQuery(GET_USER_INVITED_GROUPS)
+
 
   return (<AccountHeader>
     <MenuList>
       <MenuItem active={props.location.pathname === constants.ROUTE_ACCOUNT_PROFILE} icon={"icofont-user"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT)}>Profiili</MenuItem>
-      {familyData && familyData.getUserFamilies.length > 0 && <MenuItem active={props.location.pathname === constants.ROUTE_ACCOUNT_ELECTRICITY} icon={"icofont-energy-savings"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT_ELECTRICITY)}>Energiankulutus</MenuItem>}
+      {familyData && familyData.getUserFamilies && familyData.getUserFamilies.length > 0 && <MenuItem active={props.location.pathname === constants.ROUTE_ACCOUNT_ELECTRICITY} icon={"icofont-energy-savings"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT_ELECTRICITY)}>Energiankulutus</MenuItem>}
       <MenuItem active={props.location.pathname === constants.ROUTE_ACCOUNT_ECO} icon={"icofont-leaf"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT_ECO)}>Ekoteot</MenuItem>
-      <MenuItem alert={familyData && familyData.getUserFamilies.length > 0 && familyData.getUserFamilies.pending > 0 ? true : false} active={props.location.pathname === constants.ROUTE_ACCOUNT_FAMILY} icon={"icofont-users-alt-1"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT_FAMILY)}>Taloudet</MenuItem>
-      <MenuItem alert={groupData && groupData.getUserGroups.length > 0 && groupData.getUserGroups.pending > 0 ? true : false} active={props.location.pathname === constants.ROUTE_ACCOUNT_GROUP} icon={"icofont-users-social"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT_GROUP)}>Ryhmät</MenuItem>
+      <MenuItem alert={invitationsData && invitationsData.getUserPendingFamilies && invitationsData.getUserPendingFamilies.length > 0 ? true : false} active={props.location.pathname === constants.ROUTE_ACCOUNT_FAMILY} icon={"icofont-users-alt-1"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT_FAMILY)}>Taloudet</MenuItem>
+      <MenuItem alert={groupInvitationsData && groupInvitationsData.getUserInvitedGroups && groupInvitationsData.getUserInvitedGroups.length > 0  ? true : false} active={props.location.pathname === constants.ROUTE_ACCOUNT_GROUP} icon={"icofont-users-social"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT_GROUP)}>Ryhmät</MenuItem>
       <MenuItem active={props.location.pathname === constants.ROUTE_ACCOUNT_STATISTICS} icon={"icofont-win-trophy"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT_STATISTICS)}>Tulokset</MenuItem>
       <MenuItem active={props.location.pathname === constants.ROUTE_ACCOUNT_SETTINGS} icon={"icofont-ui-settings"} onClick={() => props.history.push(constants.ROUTE_ACCOUNT_SETTINGS)}>Asetukset</MenuItem>
     </MenuList>
